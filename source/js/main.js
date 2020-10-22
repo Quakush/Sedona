f7f08d53fe38c54c4d5c4f1d3328bf8bb9e99fcd
quakush <quakush@mail.ru>
2020-10-22 15:04:09 +0700

    js

---
+++ source/js/main.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+var searchBtn = document.querySelector('.search__btn');
+var form = document.querySelector('.search__form');
+
+console.log(searchBtn);
+console.log(form);
+
+searchBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+  }
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (!form.classList.contains('hidden')) {
+      form.classList.add('hidden');
+    }
+  }
+});
